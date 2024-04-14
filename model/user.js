c410c949d8b0ec14f176e8fd88c3ce3544655800
VerdanShandilya <108622840+VerdanShandilya@users.noreply.github.com>
2024-04-15 03:47:37 +0530

    Add files via upload

---
+++ model/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+const UserModel = mongoose.model("user", UserSchema);
+module.exports = UserModel;
